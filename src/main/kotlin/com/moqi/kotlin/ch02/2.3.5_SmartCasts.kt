2bfc78f987b26c1b0ca86b9570568ab380946977
moqi <moqimoqidea@gmail.com>
2020-12-02 16:36:36 +0800

    智能转换 Update

--- src/main/kotlin/com/moqi/kotlin/ch02/2.3.5_SmartCasts.kt
+++ src/main/kotlin/com/moqi/kotlin/ch02/2.3.5_SmartCasts.kt
@@ -13,10 +13,11 @@ class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
 fun eval(e: Expr): Int {
+
     /*if (e is Num) {
-        val n = e as Num
-        return n.value
-    }*/
+            val n = e as Num
+            return n.value
+        }*/
 
     if (e is Num) {
         return e.value
@@ -29,6 +30,26 @@ fun eval(e: Expr): Int {
     throw IllegalArgumentException("Unknown expression")
 }
 
+
+fun eval2(e: Expr): Int =
+
+    if (e is Num) {
+        e.value
+    } else if (e is Sum) {
+        eval2(e.right) + eval2(e.left)
+    } else {
+        throw IllegalArgumentException("Unknown expression")
+    }
+
+fun eval3(e: Expr): Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval3(e.right) + eval3(e.left)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
 fun main() {
     println("eval(Sum(Sum(Num(1), Num(2)), Num(4))) = ${eval(Sum(Sum(Num(1), Num(2)), Num(4)))}")
+    println("eval2(Sum(Sum(Num(1), Num(2)), Num(4))) = ${eval2(Sum(Sum(Num(1), Num(2)), Num(4)))}")
+    println("eval3(Sum(Sum(Num(1), Num(2)), Num(4))) = ${eval3(Sum(Sum(Num(1), Num(2)), Num(4)))}")
 }
