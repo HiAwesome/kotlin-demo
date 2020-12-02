c40eda4863213d316372b3ec9a95a366b6d6c019
moqi <moqimoqidea@gmail.com>
2020-12-02 16:11:32 +0800

    使用不带参数的 when

---
+++ src/main/kotlin/com/moqi/kotlin/ch02/2.3.4_WhenWithoutArument.kt
@@ -0,0 +1,30 @@
+package com.moqi.kotlin.ch02
+
+import com.moqi.kotlin.ch02.Color4.*
+import java.lang.Exception
+
+/**
+ * 使用不带参数的 when
+ *
+ * @author moqi On 12/2/20 16:09
+ */
+fun mix2(c1: Color4, c2: Color4) =
+    when {
+        (c1 == RED && c2 == YELLOW) ||
+        (c1 == YELLOW && c2 == RED) ->
+            ORANGE
+
+        (c1 == YELLOW && c2 == BLUE) ||
+        (c1 == BLUE && c2 == YELLOW) ->
+            GREEN
+
+        (c1 == BLUE && c2 == VIOLET) ||
+        (c1 == VIOLET && c2 == BLUE) ->
+            INDIGO
+
+        else -> throw Exception("Dirty color")
+    }
+
+fun main() {
+    println("mix2(BLUE, VIOLET) = ${mix2(BLUE, VIOLET)}")
+}
