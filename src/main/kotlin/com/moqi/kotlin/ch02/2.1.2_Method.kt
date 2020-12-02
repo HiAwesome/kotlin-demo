49fcdd39d510f8b172611f613005f459fa4d9f61
moqi <moqimoqidea@gmail.com>
2020-12-02 11:56:19 +0800

    函数 Update

--- src/main/kotlin/com/moqi/kotlin/ch02/2.1.2_Method.kt
+++ src/main/kotlin/com/moqi/kotlin/ch02/2.1.2_Method.kt
@@ -9,6 +9,14 @@ fun max(a: Int, b: Int): Int {
     return if (a > b) a else b
 }
 
+fun maxFun(a: Int, b: Int): Int = if (a > b) a else b
+
+fun maxFun2(a: Int, b: Int): Int = if (a > b) a else b
+
 fun main() {
     println("max(10, 100) = ${max(10, 100)}")
+    println()
+    println("maxFun(10, 20) = ${maxFun(10, 20)}")
+    println()
+    println("maxFun2(20, 30) = ${maxFun2(20, 30)}")
 }
