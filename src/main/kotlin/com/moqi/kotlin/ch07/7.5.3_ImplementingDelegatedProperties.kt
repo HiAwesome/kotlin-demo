bf9ce0beae99252ab2b8a8e265f48dbb18cdf92c
moqi <moqimoqidea@gmail.com>
2020-12-07 18:01:37 +0800

    实现委托属性 V1

---
+++ src/main/kotlin/com/moqi/kotlin/ch07/7.5.3_ImplementingDelegatedProperties.kt
@@ -0,0 +1,56 @@
+package com.moqi.kotlin.ch07.ImplementingDelegatedProperties
+
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
+
+/**
+ * 实现委托属性
+ *
+ * @author moqi On 12/7/20 17:47
+ */
+open class PropertyChangeAware {
+    protected val changeSupport = PropertyChangeSupport(this)
+
+    fun addPropertyChangeListener(listener: PropertyChangeListener) {
+        changeSupport.addPropertyChangeListener(listener)
+    }
+
+    fun removePropertyChangeListener(listener: PropertyChangeListener) {
+        changeSupport.removePropertyChangeListener(listener)
+    }
+}
+
+
+class Person(
+    val name: String, age: Int, salary: Int
+) : PropertyChangeAware() {
+
+    var age: Int = age
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+            changeSupport.firePropertyChange("age", oldValue, newValue)
+        }
+
+    var salary: Int = salary
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+            changeSupport.firePropertyChange("salary", oldValue, newValue)
+        }
+}
+
+fun main() {
+    val p = Person("Dmitry", 34, 2000)
+    p.addPropertyChangeListener(
+        PropertyChangeListener { event ->
+            println(
+                "Property ${event.propertyName} changed " +
+                        "from ${event.oldValue} to ${event.newValue}"
+            )
+        }
+    )
+
+    p.age = 35
+    p.salary = 2100
+}
