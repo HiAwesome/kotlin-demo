1dc7a308c920c275154668ebb35e31ea6fb58a81
moqi <moqimoqidea@gmail.com>
2020-12-07 19:33:08 +0800

    实现委托属性 V5

--- src/main/kotlin/com/moqi/kotlin/ch07/7.5.3_ImplementingDelegatedProperties.kt
+++ src/main/kotlin/com/moqi/kotlin/ch07/7.5.3_ImplementingDelegatedProperties.kt
@@ -6,7 +6,7 @@ import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 /**
- * 实现委托属性 V4
+ * 实现委托属性 V5
  *
  * @author moqi On 12/7/20 17:47
  */
@@ -17,20 +17,14 @@ open class PropertyChangeAware {
         changeSupport.addPropertyChangeListener(listener)
     }
 
-    fun removePropertyChangeListener(listener: PropertyChangeListener) {
-        changeSupport.removePropertyChangeListener(listener)
-    }
 }
 
 class Person(
     val name: String, age: Int, salary: Int
 ) : PropertyChangeAware() {
 
-    private val observer = {
-        prop: KProperty<*>, oldValue: Int, newValue: Int
-            ->
+    private val observer = { prop: KProperty<*>, oldValue: Int, newValue: Int ->
         changeSupport.firePropertyChange(prop.name, oldValue, newValue)
-
     }
 
     var age: Int by Delegates.observable(age, observer)
@@ -39,14 +33,12 @@ class Person(
 
 fun main() {
     val p = Person("Dmitry", 34, 2000)
-    p.addPropertyChangeListener(
-        PropertyChangeListener { event ->
-            println(
-                "Property ${event.propertyName} changed " +
-                        "from ${event.oldValue} to ${event.newValue}"
-            )
-        }
-    )
+    p.addPropertyChangeListener { event ->
+        println(
+            "Property ${event.propertyName} changed " +
+                    "from ${event.oldValue} to ${event.newValue}"
+        )
+    }
 
     p.age = 35
     p.salary = 2100
