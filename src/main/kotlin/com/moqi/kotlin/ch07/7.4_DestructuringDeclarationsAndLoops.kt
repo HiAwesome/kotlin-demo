a9b203ba5f625f25b2cb37f141be29a5a6687958
moqi <moqimoqidea@gmail.com>
2020-12-07 17:05:45 +0800

    解构声明和组件函数

---
+++ src/main/kotlin/com/moqi/kotlin/ch07/7.4_DestructuringDeclarationsAndLoops.kt
@@ -0,0 +1,36 @@
+package com.moqi.kotlin.ch07.DestructuringDeclarationsAndLoops
+
+/**
+ * 解构声明和组件函数
+ *
+ * @author moqi On 12/7/20 16:46
+ */
+fun printEntries(map: Map<String, String>) {
+    for ((key, value) in map) {
+        println("$key -> $value")
+    }
+}
+
+data class Point(val x: Int, val y: Int)
+
+data class NameComponents(val name: String, val extension: String)
+
+fun splitFilename(fullName: String): NameComponents {
+    val result = fullName.split('.', limit = 2)
+    return NameComponents(result[0], result[1])
+}
+
+fun main() {
+    val map = mapOf("oracle" to "Java", "JetBrains" to "Kotlin")
+    printEntries(map)
+    println()
+
+    val p = Point(10, 20)
+    val (x, y) = p
+    println("x = ${x}, y = ${y}")
+    println()
+
+    val (name, ext) = splitFilename("example.kt")
+    println("name = ${name}, ext = ${ext}")
+    println()
+}
