187ac90fb0459dfc2473623247701708d49ea58a
moqi <moqimoqidea@gmail.com>
2020-12-07 19:32:01 +0800

    实现委托属性 V4

--- src/main/kotlin/com/moqi/kotlin/ch07/7.5.3_ImplementingDelegatedProperties.kt
+++ src/main/kotlin/com/moqi/kotlin/ch07/7.5.3_ImplementingDelegatedProperties.kt
@@ -2,10 +2,11 @@ package com.moqi.kotlin.ch07.ImplementingDelegatedProperties
 
 import java.beans.PropertyChangeListener
 import java.beans.PropertyChangeSupport
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 /**
- * 实现委托属性 V3
+ * 实现委托属性 V4
  *
  * @author moqi On 12/7/20 17:47
  */
@@ -21,25 +22,19 @@ open class PropertyChangeAware {
     }
 }
 
-class ObservableProperty(
-    var propValue: Int,
-    val changeSupport: PropertyChangeSupport
-) {
-    operator fun getValue(p: Person, prop: KProperty<*>): Int = propValue
-
-    operator fun setValue(p: Person, prop: KProperty<*>, newValue: Int) {
-        val oldValue = propValue
-        propValue = newValue
-        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
-    }
-}
-
 class Person(
     val name: String, age: Int, salary: Int
 ) : PropertyChangeAware() {
 
-    var age: Int by ObservableProperty(age, changeSupport)
-    var salary: Int by ObservableProperty(salary, changeSupport)
+    private val observer = {
+        prop: KProperty<*>, oldValue: Int, newValue: Int
+            ->
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
+
+    }
+
+    var age: Int by Delegates.observable(age, observer)
+    var salary: Int by Delegates.observable(salary, observer)
 }
 
 fun main() {
