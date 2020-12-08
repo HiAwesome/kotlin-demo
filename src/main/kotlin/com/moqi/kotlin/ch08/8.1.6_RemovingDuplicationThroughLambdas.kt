68fcaafed68c820258d28d6e14545dc3539ef27f
moqi <moqimoqidea@gmail.com>
2020-12-08 17:32:17 +0800

    complete 通过 lambda 去除重复代码

--- src/main/kotlin/com/moqi/kotlin/ch08/8.1.6_RemovingDuplicationThroughLambdas.kt
+++ src/main/kotlin/com/moqi/kotlin/ch08/8.1.6_RemovingDuplicationThroughLambdas.kt
@@ -1,7 +1,7 @@
 package com.moqi.kotlin.ch08.RemovingDuplicationThroughLambdas
 
 /**
- * 通过 lambda 去除重复代码 V2
+ * 通过 lambda 去除重复代码
  *
  * @author moqi On 12/8/20 17:16
  */
@@ -21,11 +21,43 @@ val log = listOf(
     SiteVisit("/", 16.3, OS.ANDROID)
 )
 
+val averageWindowsDuration = log
+    .filter { it.os == OS.WINDOWS }
+    .map(SiteVisit::duration)
+    .average()
+
 fun List<SiteVisit>.averageDurationFor(os: OS) =
     filter { it.os == os }.map(SiteVisit::duration).average()
 
+fun List<SiteVisit>.averageDurationFor(predicate: (SiteVisit) -> Boolean) =
+    filter(predicate).map(SiteVisit::duration).average()
+
+val averageMobileDuration = log
+    .filter { it.os in setOf(OS.IOS, OS.ANDROID) }
+    .map(SiteVisit::duration)
+    .average()
+
 fun main() {
+    println("averageWindowsDuration = ${averageWindowsDuration}")
+    println()
+
     println("log.averageDurationFor(OS.WINDOWS) = ${log.averageDurationFor(OS.WINDOWS)}")
     println("log.averageDurationFor(OS.MAC) = ${log.averageDurationFor(OS.MAC)}")
+    println()
+
+    println("averageMobileDuration = ${averageMobileDuration}")
+    println()
+
+    println(
+        "log.averageDurationFor { it.os in setOf(OS.ANDROID, OS.IOS) } = ${
+            log.averageDurationFor {
+                it.os in setOf(
+                    OS.ANDROID,
+                    OS.IOS
+                )
+            }
+        }"
+    )
+    println("log.averageDurationFor { it.os == OS.IOS && it.path == \"/signup\" } = ${log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" }}")
     println()
 }
